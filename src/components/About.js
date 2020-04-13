941c42fff2df523f495ae3925c8436988f4b5609
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-13 11:03:44 -0700

    Finished first stage of About component styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,50 @@ import styles from './About.module.css'
 
 function About() {
     return(
-        <div className={styles.mainDiv}>ABOUT</div>
+        <div className={styles.mainDiv}>
+            <header>
+                <img src="https://avatars0.githubusercontent.com/u/41584779?s=400&u=004a3792191561f84e95e0c7761274c13b9e3d06&v=4" alt="My photo"/>
+                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </header>
+            <section className={styles.barSection}>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>JavaScript</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>CSS</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>HTML</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>ReactJS</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>React Native</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>Node</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>Firebase</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>Redux</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+                <div className={styles.barDiv}>
+                    <div className={styles.barLabel}>RWD</div>
+                    <div className={styles.barPercent}></div>
+                </div>
+            </section>
+        </div>
     )
 }
 
