b232046c7cdd8b89da824bf028ae46e890abe265
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-15 08:03:46 -0700

    Added icons in footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,23 @@ function Footer() {
     return(
         <footer className={styles.mainDiv}>
             <div className={styles.icons}>
-                <i>GitHub</i>
-                <i>LinkedIn</i>
-                <i>Facebook</i>
-                <i>E-mail</i>
+                <div className={styles.iconBox}>
+                    <i class="fas fa-code-branch"></i>
+                    <p>GitHub</p>
+                </div>
+                <div className={styles.iconBox}>
+                    <i class="fab fa-linkedin-in"></i>
+                    <p>LinkedIn</p>
+                </div>
+                <div className={styles.iconBox}>
+                    <i class="fab fa-youtube"></i>
+                    <p>YouTube</p>
+                </div>
+                <div className={styles.iconBox}>
+                    <i class="fas fa-at"></i>
+                    <p>E-mail</p>
+                </div>
+               
             </div>
             <div className={styles.infoBox}>
                 <p>Michal Zarzycki <span className={styles.year}>©2020</span></p>
