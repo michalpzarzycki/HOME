3b54024b024e49f40dc6b451a0f8f4bbb012fa82
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-17 14:28:12 -0700

    Finished styling a Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,31 +3,37 @@ import styles from './Projects.module.css'
 
 function Projects({positionY, animation}) {
     return(
-        <section className={animation.projects!==false ? styles.mainDiv : styles.none}>
+        <section className={styles.mainDiv}>
              <header>
-                <h1>PROJECTS</h1>
-                <div className={styles.line}></div>
+                <h1 className={animation.projects!==false ? styles.title : styles.none}>PROJECTS</h1>
+                <div className={animation.projects!==false ? styles.line : styles.none}></div>
             </header>
-            <section className={animation.projects!==false ? styles.projectSection : styles.none}>
-                <div className={styles.project}>
-                    <img src="https://i.pinimg.com/236x/69/16/e2/6916e2b11f37661b5ba69a422c49460c--react-tutorial-web-design-tools.jpg"/>
-                </div>
-                <div className={styles.project}>
-                    <img src="https://i.pinimg.com/236x/69/16/e2/6916e2b11f37661b5ba69a422c49460c--react-tutorial-web-design-tools.jpg"/>
-                </div>
-                <div className={styles.project}>
-                    <img src="https://i.pinimg.com/236x/69/16/e2/6916e2b11f37661b5ba69a422c49460c--react-tutorial-web-design-tools.jpg"/>
-                </div>
-                <div className={styles.project}>
-                    <img src="https://i.pinimg.com/236x/69/16/e2/6916e2b11f37661b5ba69a422c49460c--react-tutorial-web-design-tools.jpg"/>
-                </div>
-                <div className={styles.project}>
-                    <img src="https://i.pinimg.com/236x/69/16/e2/6916e2b11f37661b5ba69a422c49460c--react-tutorial-web-design-tools.jpg"/>
-                </div>
-                <div className={styles.project}>
-                    <img src="https://i.pinimg.com/236x/69/16/e2/6916e2b11f37661b5ba69a422c49460c--react-tutorial-web-design-tools.jpg"/>
-                </div>
-            </section>
+            <ul className={animation.projects!==false ? styles.projectSection : styles.none}>
+                <li className={animation.projects!==false ? styles.project : styles.none}>
+                    <div className={styles.front}></div>
+                    <div className={styles.back}>PROJECT #1 NAME</div>                
+                </li>
+                <li className={animation.projects!==false ? styles.project : styles.none}>
+                    <div className={styles.front}></div>
+                    <div className={styles.back}>PROJECT #1 NAME</div>                
+                </li>
+                <li className={animation.projects!==false ? styles.project : styles.none}>
+                    <div className={styles.front}></div>
+                    <div className={styles.back}>PROJECT #1 NAME</div>                
+                </li>
+                <li className={animation.projects!==false ? styles.project : styles.none}>
+                    <div className={styles.front}></div>
+                    <div className={styles.back}>PROJECT #1 NAME</div>                
+                </li>
+                <li className={animation.projects!==false ? styles.project : styles.none}>
+                    <div className={styles.front}></div>
+                    <div className={styles.back}>PROJECT #1 NAME</div>                
+                </li>
+                <li className={animation.projects!==false ? styles.project : styles.none}>
+                    <div className={styles.front}></div>
+                    <div className={styles.back}><span>PROJECT #1 NAME</span></div>                
+                </li>
+            </ul>
         </section>
     )
 }
