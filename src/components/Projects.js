0dafa5348e382e6ae974ad83d07d4bc347f21fe7
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-17 05:28:22 -0700

    Changed Projects animation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ function Projects({positionY, animation}) {
                 <h1>PROJECTS</h1>
                 <div className={styles.line}></div>
             </header>
-            <section className={styles.projectSection}>
+            <section className={animation.projects!==false ? styles.projectSection : styles.none}>
                 <div className={styles.project}>
                     <img src="https://i.pinimg.com/236x/69/16/e2/6916e2b11f37661b5ba69a422c49460c--react-tutorial-web-design-tools.jpg"/>
                 </div>
