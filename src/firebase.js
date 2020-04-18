b30782026cdcac47d65f83e3b2be3e6731556afe
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-18 12:32:56 -0700

    Set firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDZDJUTCJH8tFq6sZ-Exuo53QyFxNhpXlU",
+    authDomain: "portfoli-v2.firebaseapp.com",
+    databaseURL: "https://portfoli-v2.firebaseio.com",
+    projectId: "portfoli-v2",
+    storageBucket: "portfoli-v2.appspot.com",
+    messagingSenderId: "788610623560",
+    appId: "1:788610623560:web:2529754fe6ae080e67907d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
