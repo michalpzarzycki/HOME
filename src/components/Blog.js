92217b13cd735cd43aa0909dcf281281ab53a9c3
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-18 16:40:54 -0700

    Changed the way of photos displaying in Blog section

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Blog.module.css'
 
 function Blog({animation}) {
@@ -10,21 +11,35 @@ function Blog({animation}) {
                 <div className={animation.blog!==false ? styles.line : styles.none}></div>
             </header>
             <div className={styles.postDiv}>
-            <a className={styles.a} href="/">
+            <Link className={styles.a} to="/article1">
                 <div className={animation.blog!==false ? styles.postContainer : styles.none}>
                     <div className={styles.image}>
-                        <img src="https://scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/73342812_446214152753575_3115062328643551232_n.jpg?_nc_cat=110&_nc_sid=b96e70&_nc_ohc=1rHCK5P918sAX_m5WaS&_nc_ht=scontent-waw1-1.xx&_nc_tp=7&oh=84865a1b2c5fbc9d8245ba08eca9fc08&oe=5EC258D9" alt="Blog image"/>
+                    <div className={styles.background} style={{background:'url("https://scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/73342812_446214152753575_3115062328643551232_n.jpg?_nc_cat=110&_nc_sid=b96e70&_nc_ohc=1rHCK5P918sAX_m5WaS&_nc_ht=scontent-waw1-1.xx&_nc_tp=7&oh=84865a1b2c5fbc9d8245ba08eca9fc08&oe=5EC258D9")'}}></div>
+                        <div className={styles.img} style={{background:'url("https://scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/73342812_446214152753575_3115062328643551232_n.jpg?_nc_cat=110&_nc_sid=b96e70&_nc_ohc=1rHCK5P918sAX_m5WaS&_nc_ht=scontent-waw1-1.xx&_nc_tp=7&oh=84865a1b2c5fbc9d8245ba08eca9fc08&oe=5EC258D9")'}}></div>
                     </div>
                     <div className={styles.title}>My Front-End Bootcamp Story</div>
                     <div className={styles.date}>9/11/01</div>
                     <div className={styles.postLine}></div>
                     <div className={styles.description}>Description goes here</div>
                 </div>
+            </Link>
+            <a className={animation.blog!==false ? styles.a : styles.none} href="/">
+                <div className={styles.postContainer}>
+                    <div className={styles.image}>
+                    <div className={styles.background} style={{background:'url("https://i.ibb.co/2ZZNndt/hobby-ERGO.png")'}}></div>
+                        <div className={styles.img} style={{background:'url("https://i.ibb.co/2ZZNndt/hobby-ERGO.png")'}}></div>
+                    </div>
+                    <div className={styles.title}>My hobbies</div>
+                    <div className={styles.date}>9/11/01</div>
+                    <div className={styles.postLine}></div>
+                    <div className={styles.description}>Description goes here</div>
+                </div>
             </a>
             <a className={animation.blog!==false ? styles.a : styles.none} href="/">
                 <div className={styles.postContainer}>
                     <div className={styles.image}>
-                        <img src="https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Blog image"/>
+                    <div className={styles.background} style={{background:'url("https://scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/93803214_635299107319689_1694625770938302464_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=vvLe6ZxY7-8AX9KrcIb&_nc_ht=scontent-waw1-1.xx&oh=78287b26cffa3334a5a507fe3ba308be&oe=5EC1911B")'}}></div>
+                        <div className={styles.img} style={{background:'url("https://scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/93803214_635299107319689_1694625770938302464_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=vvLe6ZxY7-8AX9KrcIb&_nc_ht=scontent-waw1-1.xx&oh=78287b26cffa3334a5a507fe3ba308be&oe=5EC1911B")'}}></div>
                     </div>
                     <div className={styles.title}>WORLD TRADE CENTER</div>
                     <div className={styles.date}>9/11/01</div>
@@ -35,18 +50,8 @@ function Blog({animation}) {
             <a className={animation.blog!==false ? styles.a : styles.none} href="/">
                 <div className={styles.postContainer}>
                     <div className={styles.image}>
-                        <img src="https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Blog image"/>
-                    </div>
-                    <div className={styles.title}>WORLD TRADE CENTER</div>
-                    <div className={styles.date}>9/11/01</div>
-                    <div className={styles.postLine}></div>
-                    <div className={styles.description}>Description goes here</div>
-                </div>
-            </a>
-            <a className={animation.blog!==false ? styles.a : styles.none} href="/">
-                <div className={styles.postContainer}>
-                    <div className={styles.image}>
-                        <img src="https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Blog image"/>
+                    <div className={styles.background} style={{background:'url("https://scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/93803214_635299107319689_1694625770938302464_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=vvLe6ZxY7-8AX9KrcIb&_nc_ht=scontent-waw1-1.xx&oh=78287b26cffa3334a5a507fe3ba308be&oe=5EC1911B")'}}></div>
+                        <div className={styles.img} style={{background:'url("https://scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/93803214_635299107319689_1694625770938302464_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=vvLe6ZxY7-8AX9KrcIb&_nc_ht=scontent-waw1-1.xx&oh=78287b26cffa3334a5a507fe3ba308be&oe=5EC1911B")'}}></div>
                     </div>
                     <div className={styles.title}>WORLD TRADE CENTER</div>
                     <div className={styles.date}>9/11/01</div>
